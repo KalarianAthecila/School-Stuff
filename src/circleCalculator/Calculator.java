3c85c38886ec4e70f84842faccecd6dce1c44116
KalarianAthecila <FabianSchaefer2022@outlook.de>
2026-01-26 08:46:00 +0100

    Initial commit

---
+++ src/circleCalculator/Calculator.java
@@ -0,0 +1,47 @@
+package circleCalculator;
+
+public class Calculator {
+    int radius;
+    double pi = Math.PI;
+
+    void showRadius(){
+        // Display the radius
+        System.out.println("Radius: " + getRadius());
+    }
+
+    void setRadius(int r){
+        // Set the radius
+        radius = r;
+    }
+
+    int getRadius() {
+        // Get the radius
+        return radius;
+    }
+
+    void showArea(){
+        // Calculate and display the area
+        double area = pi * getRadius() * getRadius();
+        System.out.println("Area: " + area);
+    }
+
+    void showCircumference(){
+        // Calculate and display the circumference
+        double circumference = 2 * pi * getRadius();
+        System.out.println("Circumference: " + circumference);
+    }
+
+    void showDiameter(){
+        // Calculate and display the diameter
+        int diameter = 2 * getRadius();
+        System.out.println("Diameter: " + diameter);
+    }
+
+    void showAllProperties(){
+        // Show all properties of the circle
+        showRadius();
+        showArea();
+        showCircumference();
+        showDiameter();
+    }
+}
---
+++ src/formulaCalculator/Calculator.java
@@ -0,0 +1,35 @@
+package formulaCalculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int number1 = -3;
+        int number2 = 4;
+        int number3 = 5;
+
+        double result1 = (double)number1/number2;
+        double result2 = Math.cbrt(number2);
+        double result3 = Math.pow(number3,3);
+        double result4 = Math.sqrt(Math.pow(number2,2)+Math.pow(number3,2));
+        double result5 = (Math.sqrt(Math.pow(number2,2)+Math.pow(number3,2)))/(Math.abs(number1)+2);
+        double result6 = Math.sin((3*number1*Math.PI)/number3);
+        double result7 = ((number1*number2+Math.pow(number3,number1))/3)+(double)number2/(2+(5/(double)number2));
+
+        System.out.print("Result 1: ");
+        System.out.println(result1);
+        System.out.print("Result 2: ");
+        System.out.println(result2);
+        System.out.print("Result 3: ");
+        System.out.println(result3);
+        System.out.print("Result 4: ");
+        System.out.println(result4);
+        System.out.print("Result 5: ");
+        System.out.println(result5);
+        System.out.print("Result 6: ");
+        System.out.println(result6);
+        System.out.print("Result 7: ");
+        System.out.println(result7);
+    }
+
+}
