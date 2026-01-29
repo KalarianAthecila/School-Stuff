94a0711a2fb6407e683417b16436176c35b96c64
KalarianAthecila <FabianSchaefer2022@outlook.de>
2026-01-29 11:54:11 +0100

    Update sevenClaculator

--- src/sevenCalculator/Calculator.java
+++ src/sevenCalculator/Calculator.java
@@ -5,43 +5,43 @@ import java.util.Scanner;
 public class Calculator {
     Scanner scn = new Scanner(System.in);
 
-    int number;
     int maxNumber = 9999;
     int maxInputNumber;
-    boolean isSeven;
-    int numberRemaining;
 
     void readUpperLimit() {
-        System.out.println("Enter the upper limit (max " + maxNumber + "):");
-        maxInputNumber = scn.nextInt();
-        if (maxInputNumber > maxNumber) {
-            System.out.println("This input is not correct. Please try again.");
-            maxInputNumber = scn.nextInt();
+        while (true) {
+            System.out.println("Enter the upper limit (max " + maxNumber + "):");
+            if (scn.hasNextInt()) {
+                maxInputNumber = scn.nextInt();
+                if (maxInputNumber > 0 && maxInputNumber <= maxNumber) {
+                    break;
+                } else {
+                    System.out.println("This input is not correct. Please try again.");
+                }
+            } else {
+                System.out.println("Please enter a valid integer.");
+                scn.next(); // consume invalid input
+            }
         }
     }
 
-    void outputSeven(){
-        while (number <= maxNumber) {
-            isSeven = false;
-            numberRemaining = number;
+    void outputSeven() {
+        StringBuilder sevens = new StringBuilder();
+        for (int i = 1; i <= maxInputNumber; i++) {
+            int numberRemaining = i;
             while (numberRemaining > 0) {
                 if (numberRemaining % 10 == 7) {
-                    isSeven = true;
+                    if (!sevens.isEmpty()) sevens.append(", ");
+                    sevens.append(i);
+                    break;
                 }
                 numberRemaining = numberRemaining / 10;
             }
         }
-        if (number == 7) { // TODO: Replace with check if number is seven.
-            isSeven = true;
-        }
-        if (isSeven) {
-            System.out.println(number);
-            System.out.println("This is a seven.");
-        }
-        else {
-            System.out.println(number);
-            System.out.println("This is not a seven.");
+        if (!sevens.isEmpty()) {
+            System.out.println("Numbers containing a 7: " + sevens);
+        } else {
+            System.out.println("No numbers contain a 7.");
         }
     }
 }
-
--- src/sevenCalculator/TestCalculator.java
+++ src/sevenCalculator/TestCalculator.java
@@ -1,17 +1,9 @@
 package sevenCalculator;
 
-
-import java.util.Scanner;
-
 public class TestCalculator {
     public static void main(String[] args) {
-
-        Scanner scn = new Scanner(System.in);
-
         Calculator sevenCalculator = new Calculator();
-
         sevenCalculator.readUpperLimit();
         sevenCalculator.outputSeven();
-
     }
 }
