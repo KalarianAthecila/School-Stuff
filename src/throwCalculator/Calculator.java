40f85a2c73ede45cdadeb417c0129cb04f5af007
KalarianAthecila <FabianSchaefer2022@outlook.de>
2026-02-12 13:50:15 +0100

    Update throwCalculator

--- src/throwCalculator/Calculator.java
+++ src/throwCalculator/Calculator.java
@@ -1,6 +1,41 @@
 package throwCalculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+    Scanner scn = new Scanner(System.in);
     // Math.toRadians();
     // Math.toDegrees();
+
+    double throwHeight;
+    double throwAngle;
+    double throwSpeed;
+    double v0;
+    double x;
+    double y;
+    double xMovement;
+    double yMovement;
+    double time;
+    double gravity = 9.81f;
+    double alpha;
+
+    public void inputData(){
+        throwHeight = scn.nextDouble(); // Default: 10 meters
+        throwAngle = scn.nextDouble(); // Default: 45 degrees
+        v0 = scn.nextDouble(); // Default: 12 m/s
+    }
+
+    public void showThrowingData(){
+        while (x > 0){
+            // calculate x
+            xMovement = v0 * Math.cos(alpha) * time;
+            // calculate y
+            yMovement = v0 * Math.sin(alpha) * time - gravity / 2 * Math.pow(time, 2);
+            // check if at max height
+            // output x and y
+            System.out.println("Time: " + time + "X: " + x + "y: " + y);
+            // increase time
+        // output time, max height and range
+        }
+    }
 }
--- src/throwCalculator/TestCalculator.java
+++ src/throwCalculator/TestCalculator.java
@@ -1,5 +1,11 @@
 package throwCalculator;
 
+import throwCalculator.Calculator;
+
 public class TestCalculator {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        throwCalculator.Calculator throwCalculator = new Calculator();
+        throwCalculator.inputData();
+        throwCalculator.showThrowingData();
+    }
 }
