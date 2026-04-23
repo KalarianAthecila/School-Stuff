ec05d08d63112d1d25e9e410e62776f9c31b1feb
KalarianAthecila <FabianSchaefer2022@outlook.de>
2026-04-23 14:32:25 +0200

    Add loops

---
+++ src/loops/Loops.java
@@ -0,0 +1,30 @@
+package loops;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        ExampleLoop test = new ExampleLoop();
+        test.simpleLoop();
+        test.numbers0();
+        test.lf();
+        test.numbers1();
+        test.lf();
+        test.lf();
+        test.lf();
+        test.line(10);
+        test.emptyLine();
+        test.tripleLine(19);
+        test.lf();
+        test.block(3);
+        test.emptyLine();
+        test.triangle(4);
+        test.lf();
+        test.triangle2(4);
+        test.lf();
+        test.triangle3(4);
+        test.lf();
+        test.triangle4(4);
+        test.lf();
+        test.triangle5(4);
+    }
+}
